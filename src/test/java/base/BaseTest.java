480f5a1465cfe9728d8a0bb47707d39fd043a9ae
Ibrahim Moustafa <901935@mof.gov.sa>
2024-03-15 00:27:25 +0300

    Session11 Create Report Demo

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 import reader.ReadDataFromJson;
@@ -42,10 +43,11 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void afterMethod(Method method) throws Exception {
+    public void afterMethod(Method method, ITestResult result) throws Exception {
         utilsTests = new UtilsTests(driver);
         utilsTests.takeScreenshot(method);
         ScreenRecorderUtil.stopRecord();
+        utilsTests.setStatus(method,result);
     }
 
     @AfterClass
@@ -76,5 +78,17 @@ public class BaseTest {
             driver = new FirefoxDriver(firefoxOptions);
         }
         driver.manage().window().maximize();
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        utilsTests = new UtilsTests(driver);
+        utilsTests.createReport();
+
+    }
+    @AfterSuite
+    public void afterSuite(){
+        utilsTests = new UtilsTests(driver);
+        utilsTests.flushReport();
     }
 }
--- src/test/java/utils/UtilsTests.java
+++ src/test/java/utils/UtilsTests.java
@@ -1,9 +1,15 @@
 package utils;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +18,8 @@ import java.lang.reflect.Method;
 public class UtilsTests {
 
     WebDriver driver;
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
 
     public UtilsTests(WebDriver driver) {
         this.driver = driver;
@@ -21,5 +29,32 @@ public class UtilsTests {
 
         File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(file, new File("report/" + method.getName() + ".png"));
+    }
+
+    public void createReport() {
+        extent = new ExtentReports();
+        ExtentSparkReporter spark = new ExtentSparkReporter("report/report.html");
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("Phase5 Online Session11");
+        extent.attachReporter(spark);
+    }
+
+    public void setStatus(Method method, ITestResult testResult) {
+        test = extent.createTest(method.getName());
+        if (testResult.getStatus() == ITestResult.SUCCESS) {
+            test.pass("Test Passed");
+        } else if (testResult.getStatus() == ITestResult.FAILURE) {
+            test.fail("Test Failed");
+
+        } else {
+            test.skip("Test Skipped");
+
+        }
+        test.addScreenCaptureFromPath(method.getName() + ".png");
+        test.log(Status.INFO, "<a href='" + method.getName() + "'.avi> Download Video </a>");
+    }
+
+    public void flushReport() {
+        extent.flush();
     }
 }
